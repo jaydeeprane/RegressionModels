710e37f508017b136e156e123a9f5c4b6578d574
Jaydeep Rane <32621187+jaydeeprane@users.noreply.github.com>
2017-10-09 15:34:59 -0400

    Updated file with Regression code

--- RegressionModels.py
+++ RegressionModels.py
@@ -15,56 +15,6 @@ import statsmodels.api as sm
 from _struct import unpack
 from pickle import TRUE
 
-def CoinFlip():
-    #Initializing variables
-    
-    tails=0
-    cumulativeSum=[0]*10000
-    cumulativeProbability=[0]*10000
-    y = [0.5]*10000
-    #Simulating 10000 flips
-    for i in range(10000):
-        coinFlips=random.randint(2)#1 represents tails
-        if(coinFlips==1):
-            tails=tails+1
-            cumulativeSum[i]=tails
-            cumulativeProbability[i]=cumulativeSum[i]/(i+1)
-        else:
-            cumulativeSum[i]=tails
-            cumulativeProbability[i]=cumulativeSum[i]/(i+1)
-    
-    plot(cumulativeProbability)
-    plot(y)
-    show()     
-    
-
-
-def CLT(distribution,sampleSize,nSamples):
-    xbar = [0]*nSamples
-    
-    if (distribution == "normal" and nSamples>100 and sampleSize<=32 and sampleSize>=28):
-        for i in range(nSamples):
-            xbar[i]=mean(random.normal(50,10,sampleSize))
-     
-        title("Distribution of Sample means: ")
-        hist(xbar)
-
-        show()
-    if (distribution == "uniform" and nSamples>100 and sampleSize<=32 and sampleSize>=28): 
-        for i in range(1,nSamples):
-            xbar[i]=mean(random.uniform(0,100,sampleSize))
-        title("Distribution of Sample means: ")
-        hist(xbar)
-
-        show()
-            
-    if distribution not in ["normal", "uniform"]:
-        print("Only normal and uniform distributions are implemented")
-    if(nSamples<=100):
-        print("Please enter number of samples greater than 100")
-    if sampleSize not in (28,29,30,31,32):
-        print("Please enter sample size between 28 and 32")  
-        
 
 def SLR():
     salesdata=pd.read_csv('hw23R-Advertising.csv')
@@ -137,24 +87,3 @@ def LogisticRegressionImproved():
     modelLog="Y~X1+X2+log(X3)"
     print (sm.formula.logit(modelLog, data=df).fit().summary())
 
-#Q1
-CoinFlip()  
-
-#Q2
-CLT("normal",30,400)
-#CLT("uniform",30,500)
-#CLT("exponential",30,400)
-#CLT("normal",25,400)
-#CLT("normal",30,50)
-#CLT("exponential",24,50)
-
-#Q3
-SLR()
-
-MLR()
-
-#Q4
-LogisticRegression()
-
-#Q5
-LogisticRegressionImproved()
